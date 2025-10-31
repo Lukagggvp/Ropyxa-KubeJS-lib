10f6d7f5794d9f0e86236fd065588e146a18bf93
Ropyxa <118303452+Lukagggvp@users.noreply.github.com>
2025-10-31 19:12:12 +0200

    version 0.1
    initial version, mysterious conversion added

---
+++ startup_scripts/00_RJS_lib/1-19-2/createScript.js
@@ -0,0 +1,12 @@
+//version 0.1
+//---changelog---
+//initial version, mysterious conversion added
+
+StartupEvents.postInit(e => {
+	let $MysteriousItemConversionCategory = Java.loadClass('com.simibubi.create.compat.jei.category.MysteriousItemConversionCategory')
+	let $ConversionRecipe = Java.loadClass('com.simibubi.create.compat.jei.ConversionRecipe')
+
+	global.convertionItem = function(input, output) {
+		$MysteriousItemConversionCategory.RECIPES.add($ConversionRecipe.create(input, output))
+	}
+})
