2ce4429268517f49d7b2b7e2e889a15473af9e9f
Ropyxa <118303452+Lukagggvp@users.noreply.github.com>
2025-11-01 13:47:22 +0200

    Update recipe removal for JEI events
    Removed conversion for gold ingot to diamond.

--- client_scripts/examples-1-18-2-client.js
+++ client_scripts/examples-1-18-2-client.js
@@ -1,4 +1,4 @@
 onEvent('jei.remove.recipes', e => {
 	global.convertionItem('minecraft:iron_ingot', 'minecraft:gold_ingot')
-	global.convertionItem('minecraft:gold_ingot', 'minecraft:diamond')
+
 })
