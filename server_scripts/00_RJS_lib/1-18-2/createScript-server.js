9f2acc960b200cab4fb103bac58fc0241aeeef35
Ropyxa <118303452+Lukagggvp@users.noreply.github.com>
2025-11-03 16:57:05 +0200

    version 0.1-1.18.2-server
    initial version, added compacting

---
+++ server_scripts/00_RJS_lib/1-18-2/createScript-server.js
@@ -0,0 +1,18 @@
+//version 0.1-1.18.2-server
+//---changelog---
+//initial version, added compacting
+global.createCompacting = function(output, input, id, heat, e) {
+	e = global.TrueEvent;
+
+	e.custom({
+		type: 'create:compacting',
+		heatRequirement: heat || 'none',
+		ingredients: global.ISarrayInput(input),
+		results: global.ISarrayOutput(output)
+	}).id(id);
+};
+
+onEvent('recipes', e => {
+	global.TrueEvent = e;
+})
+
