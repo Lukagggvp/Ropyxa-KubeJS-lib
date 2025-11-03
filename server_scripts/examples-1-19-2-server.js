cd9515712e2dce27e109d682e581c408430d4a86
Ropyxa <118303452+Lukagggvp@users.noreply.github.com>
2025-11-03 12:08:16 +0200

    Add files via upload

--- server_scripts/examples-1-19-2-server.js
+++ server_scripts/examples-1-19-2-server.js
@@ -1,7 +1,14 @@
 ServerEvents.recipes(e => {
-	global.compacting(
+	global.createCompacting(
 		['10x &minecraft:water', '5x minecraft:sand', '100x &create:honey'],
 		['&#forge:milk', '9x minecraft:charcoal'],
 		'rjs_lib:compacting/some_trash'
+	);
+
+	global.createCompacting(
+		'100x &minecraft:water',
+		'1x minecraft:ice',
+		'rjs_lib:compacting/some_trash2',
+		'heated'
 	);
 });
