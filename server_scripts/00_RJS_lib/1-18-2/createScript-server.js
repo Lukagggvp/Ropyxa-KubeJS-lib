3292743d8ad549d556bd2ba9d88bf415ee47e76d
Ropyxa <118303452+Lukagggvp@users.noreply.github.com>
2025-11-05 16:30:42 +0200

    version 0.4-1.18.2-server
    added filling, emptying, deploying

--- server_scripts/00_RJS_lib/1-18-2/createScript-server.js
+++ server_scripts/00_RJS_lib/1-18-2/createScript-server.js
@@ -1,6 +1,6 @@
-//version 0.3-1.18.2-server
+//version 0.4-1.18.2-server
 //---changelog---
-//updated ISarrayInput and ISarrayOutput, added cutting
+//added filling, emptying, deploying
 global.createCompacting = function(output, input, id, heat, e) {
 	e = global.TrueEvent;
 
@@ -34,7 +34,38 @@ global.createCutting = function(output, input, id, time, e) {
 	}).id(id);
 };
 
+global.createFilling = function(output, input, id, e) {
+	e = global.TrueEvent;
+
+	e.custom({
+		type: 'create:filling',
+		ingredients: global.ISarrayInput_many_fluid_tag(input),
+		results: global.ISarrayOutput_many(output)
+	}).id(id);
+};
+
+global.createEmptying = function(output, input, id, e) {
+	e = global.TrueEvent;
+
+	e.custom({
+		type: 'create:emptying',
+		ingredients: global.ISarrayInput_many_tag(input),
+		results: global.ISarrayOutput_many_fluid(output)
+	}).id(id);
+};
+
+global.createDeploying = function(output, input, id, keep_item, e) {
+	e = global.TrueEvent;
+
+	e.custom({
+		type: 'create:deploying',
+		ingredients: global.ISarrayInput_many_tag(input),
+		keepHeldItem: keep_item || false,
+		results: global.ISarrayOutput_many(output)
+	}).id(id);
+};
+
+
 onEvent('recipes', e => {
 	global.TrueEvent = e;
 })
-
